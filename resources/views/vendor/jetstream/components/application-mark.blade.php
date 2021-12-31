4c4261a4c2d4ee8b9043ea6e4de65f7b7818254a
Argana Phangquestian <arganaphangquestian@gmail.com>
2022-01-01 01:55:58 +0700

    update: icon

---
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -0,0 +1,1 @@
+<img src="{{asset('/images/logo.jpeg')}}" alt="Kecamatan Sepatan" class="w-9 h-9 object-cover block">
---
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -0,0 +1,3 @@
+<a href="/">
+  <img src="{{asset('/images/logo.jpeg')}}" alt="Kecamatan Sepatan" class="w-16 h-16 object-contain block">
+</a>
